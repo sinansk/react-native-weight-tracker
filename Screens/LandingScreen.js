d9e0092a147d7c0ab5cfdf3948bc67b09639e095
Sinan <sinan.sk@outlook.com.tr>
2023-06-13 21:45:51 +0300

    [feature] - added language selection

--- Screens/LandingScreen.js
+++ Screens/LandingScreen.js
@@ -7,23 +7,24 @@ import { setGender, setLanguage } from '../redux/userInfoSlice';
 import * as Network from 'expo-network';
 import { useRef } from 'react';
 import BannerAdComponent from '../Components/BannerAdComponent';
+import LanguageSelectButton from '../Components/LanguageSelectButton';
 
 const LandingScreen = ({ navigation }) => {
-
     const { language } = useSelector((state) => state.userInfo)
-    const [selectedLang, setSelectedLang] = useState(language)
     const modalRef = useRef(null);
     const dispatch = useDispatch()
     const { gender } = useSelector((state) => state.userInfo)
     const [network, setNetwork] = useState()
-    const showNoConnectionMessage = !network;
+    const [showNoConnectionMessage, setShowNoConnectionMessage] = useState(false);
     const checkNetwork = async () => {
         try {
             const networkStatus = await Network.getNetworkStateAsync()
             if (networkStatus.isConnected) {
                 setNetwork(true)
+                setShowNoConnectionMessage(false)
             } else {
                 setNetwork(false)
+                setShowNoConnectionMessage(true)
             }
         }
         catch (error) {
@@ -43,17 +44,15 @@ const LandingScreen = ({ navigation }) => {
             navigation.navigate(nextScreen)
     }
 
-
-    useEffect(() => {
-        i18n.locale = selectedLang
-        dispatch(setLanguage(selectedLang))
-    }, [selectedLang])
     return (
         <View className="flex-col items-center justify-center w-full h-full bg-slate-100">
             {showNoConnectionMessage &&
                 <Text className="mb-10 text-xs font-semibold text-center text-rose-700">{i18n.t("This app requires internet connection")}</Text>
             }
-            <View className="flex-row items-center justify-center w-full gap-9 mb-9">
+            <View className="absolute top-10 right-2">
+                <LanguageSelectButton />
+            </View>
+            <View className="flex-row items-center justify-center gap-9 mb-9">
                 <TouchableOpacity onPress={() => handleGender("female")} className={`${gender === "female" && 'border-[0.3px] border-slate-500 rounded-xl bg-slate-50'} ' p-3`}>
                     <FontAwesome className="" name="female" size={gender === 'female' ? 68 : 60} color={gender === 'female' ? '#ec4899' : '#d6d3d1'} />
                 </TouchableOpacity>
