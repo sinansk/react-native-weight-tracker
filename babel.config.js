a5adb796655077c7e0ffc0b8fe1989fe9dffb688
sinansk <sinan.sk@outlook.com.tr>
2025-12-10 12:41:16 +0300

    [fix] - google play console shits

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+    presets: ["babel-preset-expo"],
+    plugins: [
       "nativewind/babel",
+      "module:react-native-dotenv",
+      "react-native-reanimated/plugin",
     ],
-    plugins: ["module:react-native-dotenv", "react-native-reanimated/plugin"],
   };
 };
