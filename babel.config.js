450e8e075af1a25d4031e2eabce0e517a9ef49e8
sinansk <sinan.sk@outlook.com.tr>
2025-12-10 16:33:53 +0300

    [fix] - google play fuck

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      "react-native-reanimated/plugin", // en sonda kalmalı
     ],
+    plugins: ["module:react-native-dotenv", "react-native-reanimated/plugin"],
   };
 };
